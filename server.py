75d3cb4fb45af1a273e5fa2ce76c39e16f7d4e51
mosrecak <maja.osrecak@gmail.com>
2025-02-20 08:01:45 -0500

    Final push

--- server.py
+++ server.py
@@ -1,32 +1,32 @@
-'''Final project assignment'''
+'''Demo Flask app for emotion detection'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emotion_analyzer():
-    '''emotion detector function    '''
+def detector():
+    '''Demo method for emotion detection'''
     text_to_analyze = request.args.get('textToAnalyze')
-    emotion_result = emotion_detector(text_to_analyze)
-    anger = emotion_result['anger']
-    disgust = emotion_result['disgust']
-    fear = emotion_result['fear']
-    joy = emotion_result['joy']
-    sadness = emotion_result['sadness']
-    dominant_emotion = emotion_result['dominant_emotion']
+    emotions = emotion_detector(text_to_analyze)
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    dominant_emotion = emotions['dominant_emotion']
 
     if dominant_emotion is None:
         return "Invalid text! Please try again"
 
-    response_str = f"""For the given statement, the system response is
+    response = f"""For the given statement, the system response is
     'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
     The dominant emotion is <strong>{dominant_emotion}</strong>."""
-    return response_str
+    return response,200
 
 @app.route("/")
 def render_index_page():
-    '''Home/index page'''
+    '''Demo index page'''
     return render_template('index.html')
 
 if __name__ == "__main__":
